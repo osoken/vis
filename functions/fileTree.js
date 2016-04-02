1c9ec0d705a50bd80c83b64a4e33fb8c7be3c3e7
osoken <osoken.devel@outlook.jp>
2016-04-02 22:50:57 +0900

    add chart list on index.jade

--- functions/fileTree.js
+++ functions/fileTree.js
@@ -1,5 +1,34 @@
 
+var fs = require('fs');
+
 !(function()
 {
-
+  var fileTree = function(p, d)
+  {
+    try
+    {
+      var stat = fs.statSync(p +'/'+ d);
+      if (stat.isDirectory())
+      {
+        return {
+          'name':d,
+          timestamp: stat.mtime.getTime(),
+          children: fs.readdirSync(p+'/'+d).map(function(dd){return fileTree(p+'/'+d, dd)})
+        };
+      }
+      if (stat.isFile())
+      {
+        return {'name':d, timestamp: stat.mtime.getTime(), size:stat.size};
+      }
+      return null;
+    }
+    catch (e)
+    {
+      console.log(e);
+      return null;
+    }
+  }
+  if (typeof define === "function" && define.amd) define(fileTree);
+  else if (typeof module === "object" && module.exports) module.exports = fileTree;
+  this.fileTree = fileTree;
 }());
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+var fileTree = require('../functions/fileTree')
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  console.log(fileTree('public','charts'));
+  res.render('index',
+             {title: 'quantized selfie',
+              tree: fileTree('public','charts')});
 });
 
 module.exports = router;
